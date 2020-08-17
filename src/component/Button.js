8b022e49b5ada21f3e61a349a3df3dde8ca0d032
Omkar Kolte <omkarkolte78@gmail.com>
2020-08-18 01:07:40 +0530

    first commit

---
+++ src/component/Button.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.css";
+
+export default class Button extends React.Component {
+  static propTypes = {
+    name: PropTypes.string,
+    red : PropTypes.bool,
+    orange: PropTypes.bool,
+    wide: PropTypes.bool,
+    clickHandler: PropTypes.func,
+  };
+
+  handleClick = () => {
+    this.props.clickHandler(this.props.name);
+  };
+
+  render() {
+    const className = [
+      "component-button",
+      this.props.red ? "red" : "",
+      this.props.blue ? "blue" : "",
+      this.props.wide ? "wide" : "",
+    ];
+
+    return (
+      <div className={className.join(" ").trim()}>
+        <button onClick={this.handleClick}>{this.props.name}</button>
+      </div>
+    );
+  }
+}
